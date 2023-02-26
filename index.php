141d6336c236a96dc1ac1954a472da9aa5b82a9c
limiteD420 <vigh.moric@gmail.com>
2023-02-26 18:58:06 +0100

    Nyolcadik

--- index.php
+++ index.php
@@ -15,8 +15,46 @@ try {
     password CHAR(60) NOT NULL,
     reg_date TIMESTAMP)";
     $pdo->exec($sql); //Létrehozom az users táblát, ha még nem lenne meg.
-    
-    $valtozo = 1;
+    echo '
+    <!DOCTYPE html>
+    <html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bejelentkezés</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:400,700">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+    <div class="login-form">
+        <form method="post" action="">
+            <h1>Bejelentkezés</h1>
+            <div class="content">
+                <div class="input-field">
+                    <input type="email" placeholder="Email" name="email" id="email">
+                </div>
+                <div class="input-field">
+                    <input type="password" placeholder="Jelszó" name="password" id="password">
+                </div>
+                <a href="#" class="link">Elfelejtetted a jelszavad?</a>
+            </div>
+            <div class="action">                
+                <button type="submit" name="login">Bejelentkezés</button>
+                <button>Regisztráció</button>
+            </div>
+        </form>
+    </div>
+           
+    </body>
+    </html>';  
+  
+$email = $_POST["email"];
+$options = ['cost' => 12,];
+$password = password_hash($_POST["password"], PASSWORD_BCRYPT, $options); //A jelszót hashelem
+
+
+echo $email;
+echo '<br>';
+echo $password;   
     
 } catch (PDOException $e) {
     echo $e->getMessage();
