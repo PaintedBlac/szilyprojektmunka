09e1570f0483348f0524b9a240132ebce7ad1b02
limiteD420 <vigh.moric@gmail.com>
2023-02-26 16:49:45 +0100

    ötödik

--- index.php
+++ index.php
@@ -3,10 +3,13 @@ include 'login.php';
 $dbname = "users";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo = new PDO("mysql:host=$host;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //ATTR_ERRMODE->hibakeresés, ERRMODE_EXCEPTION->kivételkezelés
-
-    $sql = "CREATE TABLE IF NOT EXISTS users(
+    
+    $sql = "CREATE DATABASE IF NOT EXISTS projekt_adatbazis"; //Létrehozom az adatbázis, ha még nem lenne meg.
+    $pdo->exec($sql);
+    
+    $sql = "CREATE TABLE IF NOT EXISTS projekt_adatbazis.users(
     id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     password CHAR(60) NOT NULL,
